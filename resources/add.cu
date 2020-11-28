d8c808725db80d148c5a2a1af34ee6df9e7098c1
Evan Hruskar <hruskar.evan@gmail.com>
2020-11-27 20:23:19 -0500

    Can now recompile CUDA files into ptx

--- resources/add.cu
+++ resources/add.cu
@@ -1,6 +1,7 @@
 extern "C" __constant__ int my_constant = 314;
 
 /// This is what the add.ptx is compiled from
+/// "nvcc add.cu --ptx"
 extern "C" __global__ void sum(const float* x, const float* y, float* out, int count) {
     for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < count; i += blockDim.x * gridDim.x) {
         out[i] = x[i] + y[i];
