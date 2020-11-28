368447fc7fddd02f55bef9c442599a0258eeaae2
Evan Hruskar <hruskar.evan@gmail.com>
2020-11-27 20:00:49 -0500

    Rustacuda hello world Add example

---
+++ resources/add.cu
@@ -0,0 +1,8 @@
+extern "C" __constant__ int my_constant = 314;
+
+/// This is what the add.ptx is compiled from
+extern "C" __global__ void sum(const float* x, const float* y, float* out, int count) {
+    for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < count; i += blockDim.x * gridDim.x) {
+        out[i] = x[i] + y[i];
+    }
+}
